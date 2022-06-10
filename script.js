e637bc0346a44759b6be535df193b2a92cc6eb16
zeeshanzaidi1974 <98213611+zeeshanzaidi1974@users.noreply.github.com>
2022-06-10 09:45:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-(function (window, document) {
+	
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-})(window, document);
-var LoginBtn = document.getElementById("login-btn");
-var main_page = document.getElementById("main-page");
-main_page.style.display="none";
-		
-LoginBtn.addEventListener('click' , function(){
-				 var 	Login_box = document.getElementById("login-box");
-				 var 	acc_no  = document.getElementById("acc_no") .value;
-				 var 	pass = document.getElementById("pass") .value;
-				 
-				 Login_box.style.display="none";
-				  // Login_box.style.display='none;
-				
-				 			if(acc_no==' '){ 
-										alert ( 'account number is required' ) ;
-											}
-											else if (pass==' ' ){
-													alert ( 'password is required' ) ;
-												}
-												else{
-															Login_box.style.display="none";
-															main_page.style.display="block";
-													}
-				});
-				
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})	    
